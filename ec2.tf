c49ec8d696854a9a9d2954d21afde4409f34aa8b
nagendra80 <nagendrakadumuru83@gmail.com>
2025-02-09 02:57:45 +0530

    Add ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,41 @@
+/* resource "aws_cloud9_environment_ec2" "example" {
+  instance_type = "t2.micro"
+  name          = "example-env"
+  image_id      = "amazonlinux-2023-x86_64"
+} */
+
+resource "aws_instance" "this" {
+  ami           = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-demo"
+    Purpose = "terraform-practice"
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and outbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
